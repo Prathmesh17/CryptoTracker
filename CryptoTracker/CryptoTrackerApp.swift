80fad9fbb5e761fbf52755c981975f584b609326
Prathmesh <prathmeshparteki@gmail.com>
2025-08-11 00:09:22 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Prathmesh Parteki on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
